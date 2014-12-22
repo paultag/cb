5bcc53886d470a16eea7e4a858c8758e972e7b49
Paul Tagliamonte <tag@pault.ag>
2014-12-22 15:05:07 -0500

    Initial import

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+long_description = open('README.md', 'r').read()
+
+setup(
+    name="cb",
+    version="0.1",
+    packages=['cb',],
+    package_data={'cb': ['*.hy'],},
+    author="Paul Tagliamonte",
+    author_email="paultag@debian.org",
+    long_description=long_description,
+    description='does some stuff with things & stuff',
+    license="Expat",
+    url="",
+    platforms=['any']
+)
